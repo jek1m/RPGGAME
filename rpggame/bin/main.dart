b38c650c7dd577bb1335dc0bb077eb8ffaca6ab4
jek1m <jieunkim7912@gmail.com>
2025-03-20 14:09:15 +0900

    feat : 이름 입력받기 기능 추가

---
+++ rpggame/bin/main.dart
@@ -0,0 +1,50 @@
+import 'dart:io';
+
+
+String getCharacterName() {
+  RegExp regex = RegExp(r'^[a-zA-Z가-힣]+$');
+  String? inputName;
+  while (true) {
+    print("캐릭터의 이름을 입력하세요: ");
+    inputName = stdin.readLineSync();
+    if (inputName != null && regex.hasMatch(inputName)) {
+      break;
+    } else {
+      print("잘못된 입력입니다. 특수문자나 숫자는 입력하실 수 없습니다.");
+    }
+  }
+  return inputName;
+}
+
+void main() {
+  String name = getCharacterName();
+  print(name);
+}
+
+// class Character {
+//   String? name;
+//   int? health;
+//   int? attackPower;
+//   int? defensePower;
+
+//   Character(this.name, this.health, this.attackPower, this.defensePower);
+// }
+
+// void loadCharacterStats() {
+//   try {
+//     final file = File('characters.txt');
+//     final contents = file.readAsStringSync();
+//     final stats = contents.split(',');
+//     if (stats.length != 3) throw FormatException('Invalid character data');
+      
+//     int health = int.parse(stats[0]);
+//     int attack = int.parse(stats[1]);
+//     int defense = int.parse(stats[2]);
+      
+//     String name = getCharacterName();
+//     // character = Character(name, health, attack, defense);
+//   } catch (e) {
+//     print('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+//     exit(1);
+//   }
+// }
