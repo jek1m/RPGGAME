8d614148fdcc904fd36048e52ce77d35301ead12
jek1m <jieunkim7912@gmail.com>
2025-03-20 15:44:24 +0900

    feat : character.txt 파일 읽기 기능 구현

--- rpggame/bin/main.dart
+++ rpggame/bin/main.dart
@@ -16,9 +16,51 @@ String getCharacterName() {
   return inputName;
 }
 
+class Character {
+  String? userName;
+  int? userHealth;
+  int? attackPower;
+  int? defensePower;
+
+  Character(userName, userHealth, attackPower, defensePower);
+}
+
+void loadCharacterStats() {
+
+
+  try {
+    final file = File('assets/characters.txt');
+    final contents = file.readAsStringSync();
+    final stats = contents.split(',');
+    if (stats.length != 3) throw FormatException('Invalid character data');
+      
+    int health = int.parse(stats[0]);
+    int attack = int.parse(stats[1]);
+    int defense = int.parse(stats[2]);
+      
+    String name = getCharacterName();
+    Character character = Character(name, health, attack, defense);
+    print(name);
+    print(defense);
+  } catch (e) {
+    print('캐릭터 데이터를 불러오는 데 실패했습니다: $e');
+    exit(1);
+  }
+}
+
+
 void main() {
-  String name = getCharacterName();
-  print(name);
+  //String name = getCharacterName();
+  //print(name);
+  //var file1 = File('characters.txt');
+  // var file1 = File('C:/Users/jieun/rpggame/RPGGAME/rpggame/assets/characters.txt');
+  // final contents = file1.readAsStringSync();
+  // final stats = contents.split(',');
+  // int health = int.parse(stats[0]);
+  // print(health);
+  loadCharacterStats();
+
+
 }
 
 // class Character {
